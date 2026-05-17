15b4dfdf4796572a121a08e86b1dd95c82373013
Gun Gun Priatna <gungunpriatna02@gmail.com>
2026-05-17 08:50:03 +0700

    add entry creation functionality with validation and create view

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EntryController;
 
-
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/entries', [EntryController::class, 'index']);
-Route::get('/entries/{entry}', [EntryController::class, 'show']);
 
+Route::middleware('auth')->group(function () {
+    Route::get('/entries/create', [EntryController::class, 'create']);
+    Route::post('/entries', [EntryController::class, 'store']);
+    Route::get('/entries/{entry}', [EntryController::class, 'show']);
+});
+
+// ONLY FOR DEVELOPMENT - delete after lesson 10
+Route::get('/dev-login', function () {
+    auth()->loginUsingId(1);
+    return redirect('/entries');
+});
